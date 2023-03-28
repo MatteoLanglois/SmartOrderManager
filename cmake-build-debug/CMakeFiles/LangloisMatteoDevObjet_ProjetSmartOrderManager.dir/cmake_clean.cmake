60fd2cbc9c361af92ea0b984d95f3741caaa30a5
matteolanglois <langlois.matteo49@gmail.com>
2023-03-28 15:13:17 +0200

    Commit initiale

---
+++ cmake-build-debug/CMakeFiles/LangloisMatteoDevObjet_ProjetSmartOrderManager.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LangloisMatteoDevObjet_ProjetSmartOrderManager.dir/main.cpp.obj"
+  "CMakeFiles/LangloisMatteoDevObjet_ProjetSmartOrderManager.dir/main.cpp.obj.d"
+  "LangloisMatteoDevObjet_ProjetSmartOrderManager.exe"
+  "LangloisMatteoDevObjet_ProjetSmartOrderManager.exe.manifest"
+  "LangloisMatteoDevObjet_ProjetSmartOrderManager.pdb"
+  "libLangloisMatteoDevObjet_ProjetSmartOrderManager.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LangloisMatteoDevObjet_ProjetSmartOrderManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/LangloisMatteoDevObjet_ProjetSmartOrderManager.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for LangloisMatteoDevObjet_ProjetSmartOrderManager.
+# This may be replaced when dependencies are built.
